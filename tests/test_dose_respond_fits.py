2736e08e8080991bacf4cad094e6d5a0b81851aa
Felix Hammer <fhammer@uni-osnabrueck.de>
2024-10-28 12:17:22 +0100

    better tests

--- tests/test_dose_respond_fits.py
+++ tests/test_dose_respond_fits.py
@@ -1,6 +1,4 @@
 import os
-from pathlib import Path
-import glob
 import pytest
 from sam.dose_reponse_fit import dose_response_fit, ModelPredictions, FitSettings
 from sam.plotting import plot_fit_prediction
@@ -14,17 +12,11 @@ def change_to_repo_dir():
     # Change to the repository root directory
     os.chdir(REPO_PATH)
 
-@pytest.fixture(scope="module")
-def setup_save_dir():
-    save_dir = "control_imgs/fits"
-    os.makedirs(save_dir, exist_ok=True)
-    return save_dir
 
 @pytest.mark.parametrize("file", load_files())
-def test_dose_response_fit_and_plot(file, setup_save_dir):
+def test_dose_response_fit_and_plot(file):
     path, data = file
     
-    save_dir = setup_save_dir
     data: ExperimentData = read_data(path)
 
     frozen_inputs = deepcopy(data.main_series)
@@ -42,8 +34,4 @@ def test_dose_response_fit_and_plot(file, setup_save_dir):
     fig = plot_fit_prediction(model=res, title=title)
 
     # Save the plot
-    save_path = os.path.join(save_dir, f"{os.path.split(path.replace('xlsx', 'png'))[1]}")
-    fig.savefig(save_path)
     plt.close()
-    # Check if the plot was saved
-    assert os.path.exists(save_path), f"Plot was not saved for {path}"
